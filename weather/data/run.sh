44196f9d40b7ce574ad9cce6055cd8168c21238c
Branislav Holý <branislav.holy@gmail.com>
2026-03-31 23:44:53 +0200

    feat: add weather ha variables

--- weather/data/run.sh
+++ weather/data/run.sh
@@ -4,8 +4,11 @@ echo "Starting weather service..."
 
 export HA_LANGUAGE=${HA_LANGUAGE:-$(bashio::config 'language')}
 export HA_UNITS=${HA_UNITS:-$(bashio::config 'uof')}
+export MQTT_USERNAME=${MQTT_USERNAME:-$(bashio::config 'username')}
+export MQTT_PASSWORD=${MQTT_PASSWORD:-$(bashio::config 'password')}
 
 echo "Language: '$HA_LANGUAGE'"
 echo "Units: '$HA_UNITS'"
+echo "Username: '$MQTT_USERNAME'"
 
 exec /usr/bin/weather
