99b51c5fa59ae9e414393c798ed96fd57e756001
Branislav Holý <49227340+branislavholy@users.noreply.github.com>
2026-04-01 23:21:12 +0200

    chore(main): release 1.2.4

--- weather/data/main.go
+++ weather/data/main.go
@@ -32,8 +32,8 @@ var messagePubHandler mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Me
 }
 
 var (
-	// BUILD_VERSION = "1.2.3" // x-release-please-version
-	version = "1.2.3" // x-release-please-version
+	// BUILD_VERSION = "1.2.4" // x-release-please-version
+	version = "1.2.4" // x-release-please-version
 	commit  = "none"
 	date    = "unknown"
 	// githubUrl = "none"
