9ac64abea743fc46a6da611a087833d226c4bccc
Branislav Holý <branislav.holy@gmail.com>
2026-04-08 19:45:39 +0200

    fix(weather): init

--- weather/data/run.sh
+++ weather/data/run.sh
@@ -4,7 +4,6 @@ echo "Starting weather service..."
 
 export HA_LANGUAGE=${HA_LANGUAGE:-$(bashio::config 'language')}
 export HA_UNITS=${HA_UNITS:-$(bashio::config 'uof')}
-# export MQTT_HOSTNAME=${MQTT_HOSTNAME:-$(bashio::config 'hostname')}
 export MQTT_HOSTNAME=${MQTT_HOSTNAME:-$(bashio::info.hostname)}
 export MQTT_PASSWORD=${MQTT_PASSWORD:-$(bashio::config 'password')}
 export MQTT_PORT=${MQTT_PORT:-1883}
