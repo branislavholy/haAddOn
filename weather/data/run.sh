352d18d3a13edd221e9a5601bbf7e991e4cf315e
Branislav Holý <branislav.holy@gmail.com>
2026-04-08 17:12:22 +0200

    fix: update MQTT default host and port

--- weather/data/run.sh
+++ weather/data/run.sh
@@ -4,15 +4,17 @@ echo "Starting weather service..."
 
 export HA_LANGUAGE=${HA_LANGUAGE:-$(bashio::config 'language')}
 export HA_UNITS=${HA_UNITS:-$(bashio::config 'uof')}
-export MQTT_HOSTNAME=${MQTT_HOSTNAME:-$(bashio::config 'hostname')}
-export MQTT_PORT=${MQTT_PORT:-$(bashio::config 'port')}
-export MQTT_USERNAME=${MQTT_USERNAME:-$(bashio::config 'username')}
+# export MQTT_HOSTNAME=${MQTT_HOSTNAME:-$(bashio::config 'hostname')}
+export MQTT_HOSTNAME=${MQTT_HOSTNAME:-$(bashio::services mqtt "host"}
 export MQTT_PASSWORD=${MQTT_PASSWORD:-$(bashio::config 'password')}
+# export MQTT_PORT=${MQTT_PORT:-$(bashio::config 'port')}
+export MQTT_PORT=${MQTT_PORT:-$(bashio::services mqtt "port"}
+export MQTT_USERNAME=${MQTT_USERNAME:-$(bashio::config 'username')}
 
-echo "Loaded language: '$HA_LANGUAGE'"
-echo "Loaded units:    '$HA_UNITS'"
 echo "Loaded hostname: '$MQTT_HOSTNAME'"
+echo "Loaded language: '$HA_LANGUAGE'"
 echo "Loaded port:     '$MQTT_PORT'"
+echo "Loaded units:    '$HA_UNITS'"
 echo "Loaded username: '$MQTT_USERNAME'"
 
 exec /usr/bin/weather
