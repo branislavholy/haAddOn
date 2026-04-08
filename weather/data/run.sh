086052e6c8e24146cfc03aad0f5066338268a612
Branislav Holý <branislav.holy@gmail.com>
2026-04-08 17:37:18 +0200

    fix: update MQTT default host and port

--- weather/data/run.sh
+++ weather/data/run.sh
@@ -5,10 +5,9 @@ echo "Starting weather service..."
 export HA_LANGUAGE=${HA_LANGUAGE:-$(bashio::config 'language')}
 export HA_UNITS=${HA_UNITS:-$(bashio::config 'uof')}
 # export MQTT_HOSTNAME=${MQTT_HOSTNAME:-$(bashio::config 'hostname')}
-export MQTT_HOSTNAME=${MQTT_HOSTNAME:-$(bashio::services mqtt "host")}
+export MQTT_HOSTNAME=${MQTT_HOSTNAME:-$(bashio::info.hostname)}
 export MQTT_PASSWORD=${MQTT_PASSWORD:-$(bashio::config 'password')}
-# export MQTT_PORT=${MQTT_PORT:-$(bashio::config 'port')}
-export MQTT_PORT=${MQTT_PORT:-$(bashio::services mqtt "port")}
+export MQTT_PORT=${MQTT_PORT:-1883}
 export MQTT_USERNAME=${MQTT_USERNAME:-$(bashio::config 'username')}
 
 echo "Loaded hostname: '$MQTT_HOSTNAME'"
