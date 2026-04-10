9cbfe4f48c7a5de23fe77aee80c1ad8585fc6d8f
Branislav Holý <49227340+branislavholy@users.noreply.github.com>
2026-04-11 00:41:06 +0200

    chore(main): release 1.5.1

--- weather/data/main.go
+++ weather/data/main.go
@@ -35,7 +35,7 @@ var messagePubHandler mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Me
 }
 
 var (
-	version = "1.5.0" // x-release-please-version
+	version = "1.5.1" // x-release-please-version
 	// Define by GoReleaser
 	commit = "none"
 	date   = "unknown"
